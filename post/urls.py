f761516ab493fa5ba59e1456b7d211a1e24fc4fd
Salman <salmansiddik19@gmail.com>
2021-02-07 13:07:18 +0600

    commit

--- post/urls.py
+++ post/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, AddPostView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list_view'),
     path('<int:pk>', PostDetailView.as_view(), name='post_detail_view'),
+    path('add_post', AddPostView.as_view(), name='add_post_view'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Post
+from .forms import PostForm
 
 
 class PostListView(generic.ListView):
@@ -11,3 +12,9 @@ class PostListView(generic.ListView):
 class PostDetailView(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+class AddPostView(generic.CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
