591960bfc9a8e6c0c6cc9792cf7724c1247482e2
Salman <salmansiddik19@gmail.com>
2021-02-13 16:30:03 +0600

    commit

--- post/models.py
+++ post/models.py
@@ -20,6 +20,20 @@ class Category(models.Model):
         return reverse('post_list_view')
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, null=True, blank=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_picture = models.ImageField(
+        null=True, blank=True, upload_to='images/profile')
+
+    class Meta:
+        db_table = 'profiles'
+
+    def __str__(self):
+        return str(self.user)
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     image = models.ImageField(null=True, blank=True, upload_to='images/')
